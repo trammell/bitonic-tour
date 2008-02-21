7ee3e61f54ae0d2c632a943b828602c197394ff5
johnt <johnt@5bda1b4c-df04-11dc-9541-bd5b2d3e4f05>
2008-02-21 22:28:45 +0000

    adding sample code to solve the Cormen problem
    git-svn-id: file:///Volumes/JJT 4GB/Apps/svn_repo/Cormen-Bitonic/trunk@21 5bda1b4c-df04-11dc-9541-bd5b2d3e4f05

---
+++ example/cb.pl
@@ -0,0 +1,13 @@
+#!perl
+
+use strict;
+use warnings;
+use Cormen::Bitonic;
+
+my $b = Cormen::Bitonic->new;
+while (<>) {
+    next if /^#/;
+    next unless /\S/;
+    $b->add_point(split ' ', $_);
+}
+
