fb30fcfb5add4f15542bd7f8b9bd15bbd5337a81
johnt <johnt@5bda1b4c-df04-11dc-9541-bd5b2d3e4f05>
2008-02-21 15:20:29 +0000

    import
    git-svn-id: file:///Volumes/JJT 4GB/Apps/svn_repo/Cormen-Bitonic/trunk@17 5bda1b4c-df04-11dc-9541-bd5b2d3e4f05

---
+++ lib/Cormen/Bitonic.pm
@@ -0,0 +1,89 @@
+package Cormen::Bitonic;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Cormen::Bitonic - solution to the 
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+    use Cormen::Bitonic;
+
+    my $b = Cormen::Bitonic->new;
+
+=head1 METHODS
+
+=head2 new()
+
+=cut
+
+sub new {
+
+}
+
+=head1 AUTHOR
+
+John Trammell, C<< <johntrammell at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-cormen-bitonic at rt.cpan.org>,
+or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Cormen-Bitonic>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Cormen::Bitonic
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Cormen-Bitonic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Cormen-Bitonic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Cormen-Bitonic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Cormen-Bitonic>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 John Trammell, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
