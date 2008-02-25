18a64fedb53c39926c7959c988d27304d590d45c
johnt <johnt@5bda1b4c-df04-11dc-9541-bd5b2d3e4f05>
2008-02-25 22:47:51 +0000

    more name conversion
    git-svn-id: file:///Volumes/JJT 4GB/Apps/svn_repo/Algorithm-TravelingSalesman-BitonicTour/trunk@43 5bda1b4c-df04-11dc-9541-bd5b2d3e4f05

--- example/cb.pl
+++ example/cb.pl
@@ -5,11 +5,17 @@ use warnings;
 use Algorithm::TravelingSalesman::BitonicTour;
 
 my $b = Algorithm::TravelingSalesman::BitonicTour->new;
-while (<>) {
-    next if /^#/;
-    next unless /\S/;
-    $b->add_point(split ' ', $_);
-}
+
+# Grid points from Cormen, Figure 15.9, p. 365.
+# Note that the points don't need to be added in left-to-right order.
+
+$b->add_point(0,6);
+$b->add_point(5,4);
+$b->add_point(7,5);
+$b->add_point(8,2);
+$b->add_point(6,1);
+$b->add_point(1,0);
+$b->add_point(2,3);
 
 print $b->solve;
 
