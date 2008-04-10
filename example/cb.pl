4d293d017b6a5e94469e0fb0ac670e2c4c321299
johnt <johnt@5bda1b4c-df04-11dc-9541-bd5b2d3e4f05>
2008-04-10 18:05:29 +0000

    consolidating
    git-svn-id: file:///Volumes/JJT 4GB/Apps/svn_repo/Algorithm-TravelingSalesman-BitonicTour/trunk@84 5bda1b4c-df04-11dc-9541-bd5b2d3e4f05

--- example/cb.pl
+++ example/cb.pl
@@ -6,16 +6,16 @@ use Algorithm::TravelingSalesman::BitonicTour;
 
 my $b = Algorithm::TravelingSalesman::BitonicTour->new;
 
-# Grid points from Cormen, Figure 15.9, p. 365.
-# Note that the points don't need to be added in left-to-right order.
+# Grid points from Cormen, Figure 15.9, p. 365.  Note that points can be added
+# in any order; these just happen to be left-to-right.
 
 $b->add_point(0,6);
-$b->add_point(5,4);
-$b->add_point(7,5);
-$b->add_point(8,2);
-$b->add_point(6,1);
 $b->add_point(1,0);
 $b->add_point(2,3);
+$b->add_point(5,4);
+$b->add_point(6,1);
+$b->add_point(7,5);
+$b->add_point(8,2);
 
 print $b->solve;
 
